7cc521ac564190fa8bb4300f7007d18cf388e92f
Ziad Abouelfarah <zain.fake02@gmail.com>
2020-01-29 23:54:42 +0100

    Reusing the Backdrop.mp4

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,28 @@ import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import Sidebar from "../Navigation/SideDrawer/SideDrawer";
 
-export default props => {
-  return (
-    <React.Fragment>
-      <Toolbar />
-      <Sidebar />
-      {/* <div>Toolbar, sidebare and other stuff</div> */}
-      <main className={classes.Contain}>{props.children}</main>
-    </React.Fragment>
-  );
-};
+class Layout extends React.Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <Sidebar
+          show={this.state.showSideDrawer}
+          closed={this.sideDrawCloseHandler}
+        />
+        {/* <div>Toolbar, sidebare and other stuff</div> */}
+        <main className={classes.Contain}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,27 @@ import React from "react";
 import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
 import Logo from "../../Logo/Logo";
 import classes from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 export default props => {
-  return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
-      </div>
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.show) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
 
-      <nav>
-        <NavigationItem>Bossa l5ot</NavigationItem>
-        <NavigationItem>League of BARGAAr</NavigationItem>
-      </nav>
-    </div>
+  return (
+    <React.Fragment>
+      <Backdrop show={props.show} closeModal={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItem>Bossa l5ot</NavigationItem>
+          <NavigationItem>League of BARGAAr</NavigationItem>
+        </nav>
+      </div>
+    </React.Fragment>
   );
 };
